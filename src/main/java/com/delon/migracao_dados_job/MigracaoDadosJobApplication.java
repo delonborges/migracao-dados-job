3192ed6d56f2c8969ce947c35a28759c565fb314
Delon Borges <delon.borges@icloud.com>
2024-06-22 19:13:38 -0300

    feat: first commit

---
+++ src/main/java/com/delon/migracao_dados_job/MigracaoDadosJobApplication.java
@@ -0,0 +1,13 @@
+package com.delon.migracao_dados_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracaoDadosJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigracaoDadosJobApplication.class, args);
+    }
+
+}
